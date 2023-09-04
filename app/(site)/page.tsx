5606c04da451b5132cf7735d51a32a92ecff2103
Tuomas <tuomas.orhanen@amban.fi>
2023-09-04 23:54:32 +0300

    no sorting

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,35 +2,21 @@
 import { useEffect, useState } from 'react';
 import { getProjects, deleteProject } from "@/sanity/sanity-utils";
 import Link from 'next/link';
-import { Project } from "@/types/Types";
+import { Project } from "@/types/Types";  // Import the Project type
 
 export default function Home() {
-  const [projects, setProjects] = useState<Project[]>([]);
-  const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
+  const [projects, setProjects] = useState<Project[]>([]);  // Add type annotation
 
   useEffect(() => {
     const fetchData = async () => {
       const proj = await getProjects();
-      const sortedProjects = sortProjects(proj, sortOrder);
-      setProjects(sortedProjects);
+      setProjects(proj);
     };
 
     fetchData();
-  }, [sortOrder]);
+  }, []);
 
-  const sortProjects = (projects: Project[], order: 'asc' | 'desc') => {
-    return [...projects].sort((a, b) => {
-      const dateA = new Date(a.createdAt);
-      const dateB = new Date(b.createdAt);
-      return order === 'asc' ? dateA.getTime() - dateB.getTime() : dateB.getTime() - dateA.getTime();
-    });
-  };
-
-  const handleSortToggle = () => {
-    setSortOrder(prevOrder => prevOrder === 'asc' ? 'desc' : 'asc');
-  };
-
-  const handleDelete = async (id: string) => {
+  const handleDelete = async (id: string) => {  // Add type annotation for id
     if (window.confirm('Are you sure you want to delete this post?')) {
       await deleteProject(id);
       setProjects(projects.filter(project => project._id !== id));
@@ -40,11 +26,6 @@ export default function Home() {
   return (
     <div>
       <h2 className="font-bold text-3xl">Julkaisut</h2>
-      <label htmlFor="sortOrder">Sort by: </label>
-      <select id="sortOrder" value={sortOrder} onChange={(e) => setSortOrder(e.target.value as 'asc' | 'desc')}>
-        <option value="asc">Oldest</option>
-        <option value="desc">Newest</option>
-      </select>
       <div className="mt-5">
         {projects.map((project) => (
           <div key={project._id} className="pb-2 border-b text-xl flex justify-between">
@@ -55,6 +36,7 @@ export default function Home() {
             </Link>
             <button className='hover:scale-110' onClick={() => handleDelete(project._id)}>X</button>
           </div>
+          
         ))}
       </div>
     </div>
