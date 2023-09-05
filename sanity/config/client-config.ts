8b1b55eefb73c152de38480602411f598308fedc
Tuomas <tuomas.orhanen@amban.fi>
2023-09-05 15:21:58 +0300

    use cdn false

--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -2,6 +2,7 @@ const config = {
   projectId: "x8b0csby",
   dataset: "production",
   apiVersion: "2023-03-09",
+  useCdn: false,
   token: "skCJks3JpoQ2a2msNLnLNMlVKzkJ9jJd6CyRlAwYonihcKFqJ5gvC587IzLhNBd3G0lXtBP0PI7jSc45Me7kIbnyIK8wXK43AkAw6Jytqgq61MX8pqWD4W8xcE6jq0WBOJBaNtrtT6snMU75Ne14hD2x7UOIq4HFiRSybdUNk7lIFCU17M9t"
 }
 
