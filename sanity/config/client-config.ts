a4dd884f041b722e31f4a65a577a0b7c76c0eefe
Tuomas Orhanen <92747067+tuomasorhanen@users.noreply.github.com>
2024-03-25 15:45:03 +0200

    Update client-config.ts

--- sanity/config/client-config.ts
+++ sanity/config/client-config.ts
@@ -1,9 +1,9 @@
 const config = {
-  projectId: "x8b0csby",
+  projectId: "",
   dataset: "production",
   apiVersion: "2023-03-09",
   useCdn: false,
-  token: "skCJks3JpoQ2a2msNLnLNMlVKzkJ9jJd6CyRlAwYonihcKFqJ5gvC587IzLhNBd3G0lXtBP0PI7jSc45Me7kIbnyIK8wXK43AkAw6Jytqgq61MX8pqWD4W8xcE6jq0WBOJBaNtrtT6snMU75Ne14hD2x7UOIq4HFiRSybdUNk7lIFCU17M9t"
+  token: ""
 }
 
 export default config;
